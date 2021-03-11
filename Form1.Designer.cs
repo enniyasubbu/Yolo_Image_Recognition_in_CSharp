4a6a083cfdabf3b32af2aa515d7cc9c293131abc
Enniyan Thangaraju <enniyasubbu@outlook.com>
2021-03-11 01:07:36 +0100

    https://github.com/AlturosDestinations/Alturos.Yolo.git
    Please download and extract the files and do data binding source on your application

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace DetectObjectApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.yoloTrackingItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.confidenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.widthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.heightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yoloItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloTrackingItemBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(12, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1056, 554);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(1142, 31);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 45);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(1142, 107);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(100, 52);
+            this.btnDetect.TabIndex = 2;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.typeDataGridViewTextBoxColumn,
+            this.confidenceDataGridViewTextBoxColumn,
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn,
+            this.widthDataGridViewTextBoxColumn,
+            this.heightDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.yoloItemBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 572);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1056, 125);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // yoloTrackingItemBindingSource
+            // 
+            this.yoloTrackingItemBindingSource.DataSource = typeof(Alturos.Yolo.Model.YoloTrackingItem);
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            this.typeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // confidenceDataGridViewTextBoxColumn
+            // 
+            this.confidenceDataGridViewTextBoxColumn.DataPropertyName = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.HeaderText = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.confidenceDataGridViewTextBoxColumn.Name = "confidenceDataGridViewTextBoxColumn";
+            this.confidenceDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            this.xDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            this.yDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // widthDataGridViewTextBoxColumn
+            // 
+            this.widthDataGridViewTextBoxColumn.DataPropertyName = "Width";
+            this.widthDataGridViewTextBoxColumn.HeaderText = "Width";
+            this.widthDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.widthDataGridViewTextBoxColumn.Name = "widthDataGridViewTextBoxColumn";
+            this.widthDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // heightDataGridViewTextBoxColumn
+            // 
+            this.heightDataGridViewTextBoxColumn.DataPropertyName = "Height";
+            this.heightDataGridViewTextBoxColumn.HeaderText = "Height";
+            this.heightDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.heightDataGridViewTextBoxColumn.Name = "heightDataGridViewTextBoxColumn";
+            this.heightDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // yoloItemBindingSource
+            // 
+            this.yoloItemBindingSource.DataSource = typeof(Alturos.Yolo.Model.YoloItem);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 726);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YoloCSharp";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloTrackingItemBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn confidenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn widthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn heightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource yoloTrackingItemBindingSource;
+        private System.Windows.Forms.BindingSource yoloItemBindingSource;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using Alturos.Yolo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DetectObjectApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "PNG|*.png|JPEG|*.jpeg"})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pic.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                using(MemoryStream ms = new MemoryStream())
+                {
+                    pic.Image.Save(ms, ImageFormat.Png);
+                    var items = yoloWrapper.Detect(ms.ToArray());
+                    yoloItemBindingSource.DataSource = items;
+                }
+                
+                //items[0].Type -> "Person, Car, ..."
+                //items[0].Confidence -> 0.0 (low) -> 1.0 (high)
+                //items[0].X -> bounding box
+                //items[0].Y -> bounding box
+                //items[0].Width -> bounding box
+                //items[0].Height -> bounding box
+            }
+
+        }
+    }
+}
